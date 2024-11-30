3daf724e0daa09a583c33635b687bea519ff9f32
tiagoefreitas <tiagofreitas87@gmail.com>
2024-11-30 02:37:34 +0000

    fix: correct webhooks import path

--- backend/utils/plugins.py
+++ backend/utils/plugins.py
@@ -17,7 +17,7 @@ from models.chat import Message
 from models.memory import Memory, MemorySource
 from models.notification_message import NotificationMessage
 from models.plugin import Plugin, UsageHistoryType
-from omi.backend.utils.webhooks import notifications_history_webhook
+from utils.webhooks import notifications_history_webhook
 from utils.apps import get_available_apps, weighted_rating
 from utils.notifications import send_notification
 from utils.other.endpoints import timeit
